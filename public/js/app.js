5ee9253ce87495018b042d79ac8438cebe557cc5
komali2 <rogersjcaleb@gmail.com>
2016-08-15 11:35:58 -0700

    core: fixed appjs

--- public/js/app.js
+++ public/js/app.js
@@ -1,116 +1,116 @@
-angular.module('contactsApp', ['ngRoute'])
-    .config(function($routeProvider){
+angular.module("contactsApp", ['ngRoute'])
+    .config(function($routeProvider) {
         $routeProvider
-        .when("/", {
-            templateUrl: "list.html",
-            controller: "ListController",
-            resolve: {
-                contacts: function(Contacts){
-                    return Contacts.getContacts();
+            .when("/", {
+                templateUrl: "list.html",
+                controller: "ListController",
+                resolve: {
+                    contacts: function(Contacts) {
+                        return Contacts.getContacts();
+                    }
                 }
-            }
-        })
-        .when('/new/contact', {
-            controller: 'NewContactController',
-            templateurl: 'contact-form.html'
-        })
-        .when('/contact/:contactId', {
-            controller: 'EditContactController',
-            templateUrl: 'contact.html'
-        })
-        .otherwise({
-            redirectTo: '/'
-        })
+            })
+            .when("/new/contact", {
+                controller: "NewContactController",
+                templateUrl: "contact-form.html"
+            })
+            .when("/contact/:contactId", {
+                controller: "EditContactController",
+                templateUrl: "contact.html"
+            })
+            .otherwise({
+                redirectTo: "/"
+            })
     })
-    .service("Contacts", function($http){
-        this.getContacts = function(){
-            return $http.get("/contacts")
-            .then((res)=>{
-                return res;
-            }, (res)=>{
-                alert("Err retreiving contacts");
-            });
+    .service("Contacts", function($http) {
+        this.getContacts = function() {
+            return $http.get("/contacts").
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error finding contacts.");
+                });
         }
-        this.createContact = function(contact){
-            return $http.post('/contacts', contact)
-            .then((res)=>{
-                return response;
-            }, (err)=>{
-                alert('Error creating contact');
-            });
+        this.createContact = function(contact) {
+            return $http.post("/contacts", contact).
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error creating contact.");
+                });
         }
-        this.getContact = function(contactId){
-            var url = '/contacts/' + contactId;
-            return $http.get(url)
-                .then((res)=>{
-                    return res;
-                }, (err)=>{
+        this.getContact = function(contactId) {
+            var url = "/contacts/" + contactId;
+            return $http.get(url).
+                then(function(response) {
+                    return response;
+                }, function(response) {
                     alert("Error finding this contact.");
                 });
         }
-        this.editContact = function(contact){
-            var url = '/contacts/' + contact._id;
+        this.editContact = function(contact) {
+            var url = "/contacts/" + contact._id;
             console.log(contact._id);
-            return $http.put(url, contact)
-                .then((res)=>{
-                    return res;
-                }, (err)=>{
-                    alert("error editing this contact.");
-                    console.log(err);
+            return $http.put(url, contact).
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error editing this contact.");
+                    console.log(response);
                 });
         }
-        this.deleteContact = function(contactId){
-            var url = '/contacts/' + contactId;
-            return $http.delete(url)
-                .then((res)=>{
-                    return res;
-                }, (err)=>{
-                    alert('Error deleting this contact.');
-                    console.log(err);
+        this.deleteContact = function(contactId) {
+            var url = "/contacts/" + contactId;
+            return $http.delete(url).
+                then(function(response) {
+                    return response;
+                }, function(response) {
+                    alert("Error deleting this contact.");
+                    console.log(response);
                 });
         }
     })
-    .controller("ListController", (contacts, $scope)=>{
+    .controller("ListController", function(contacts, $scope) {
         $scope.contacts = contacts.data;
     })
-    .controller('NewContactController', ($scope, $location, Contacts)=>{
-        $scope.back = function(){
-            $location.path('#/');
+    .controller("NewContactController", function($scope, $location, Contacts) {
+        $scope.back = function() {
+            $location.path("#/");
         }
 
-        $scope.saveContact = function(contact){
-            Contacts.createContact(contact).then((doc)=>{
-                var contactUrl = '/contact/' + doc.data._id;
+        $scope.saveContact = function(contact) {
+            Contacts.createContact(contact).then(function(doc) {
+                var contactUrl = "/contact/" + doc.data._id;
                 $location.path(contactUrl);
-            }, (err)=>{
-                alert(err);
+            }, function(response) {
+                alert(response);
             });
         }
     })
-    .controller('EditContactController', ($scope, $routeParams, Contacts)=>{
-        Contacts.getContact($routeParams.contactId).then((doc)=>{
+    .controller("EditContactController", function($scope, $routeParams, Contacts) {
+        Contacts.getContact($routeParams.contactId).then(function(doc) {
             $scope.contact = doc.data;
-        }, (err)=>{
-            alert(err);
+        }, function(response) {
+            alert(response);
         });
 
-        $scope.toggleEdit = function(){
+        $scope.toggleEdit = function() {
             $scope.editMode = true;
-            $scope.contactFormUrl = 'contact-form.html';
+            $scope.contactFormUrl = "contact-form.html";
         }
 
-        $scope.back = function(){
+        $scope.back = function() {
             $scope.editMode = false;
-            $scope.contactFormUrl = '';
+            $scope.contactFormUrl = "";
         }
 
-        $scope.saveContact = function(contact){
+        $scope.saveContact = function(contact) {
             Contacts.editContact(contact);
             $scope.editMode = false;
-            $scope.contactFormUrl = '';
+            $scope.contactFormUrl = "";
         }
 
-        $scope.deleteContact = function(contactId){
+        $scope.deleteContact = function(contactId) {
             Contacts.deleteContact(contactId);
         }
     });
