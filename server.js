c310b00a88190c05a1031641e11b0f1482faf10b
komali2 <rogersjcaleb@gmail.com>
2016-08-14 21:23:31 -0700

    added handler for post contact route

--- server.js
+++ server.js
@@ -34,3 +34,57 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, (err, database)=>{
         console.log('App now running on port,', port);
     });
 });
+
+//API ROUTES
+
+//basic error handle function
+function handleError(res, reason, message, code){
+    console.log('ERROR: ' + reason);
+    res.status(code || 500).json({"error": message});
+}
+
+/*
+    "/contacts"
+    GET: finds all contacts
+    POST: creates a new contact
+*/
+
+app.get('/contacts', (req, res)=>{
+
+});
+
+app.post('/contacts', (req, res)=>{
+    let newContact = req.body;
+    newContact.createDate = new Date();
+
+    if(!(req.body.firstName || req.body.lastName)){
+        handleError(res, 'Invalid User Input', 'Must provide a first and last name', 400);
+    }
+
+    db.collection(CONTACTS_COLLECTION).insertOne(newContact, (err, doc)=>{
+        if(err){
+            handleError(res, err.message, "Failed to create new contact");
+        } else{
+            res.status(201).json(doc.ops[0]);
+        }
+    });
+});
+
+/*
+    "/contacts/:id"
+    GET: find contact by id
+    PUT: update contact by id
+    DELETE: deletes contact by id
+*/
+
+app.get('/contacts/:id', (req, res)=>{
+
+});
+
+app.put('/contacts/:id', (req, res)=>{
+
+});
+
+app.delete('/contacts/:id', (req, res)=>{
+
+})
